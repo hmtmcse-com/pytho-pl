41c5fe9941ca2375b24fe1f83eca44008433c52e
hmtmcse <hmtm.cse@gmail.com>
2021-05-21 21:48:59 +0600

    comment

---
+++ data_holder.py
@@ -0,0 +1,18 @@
+string_data = "This is String Data"
+string_data_other = 'This is also String Data'
+string_data_with_type: str = 'This is String Data with data type'
+
+integer_data = 100
+integer_data_with_type: int = 100
+
+float_data = 100.10
+float_data_with_type: float = 100.10
+
+double_data = 100.00000001
+
+boolean_data = True
+boolean_data_with_type: bool = True
+
+
+PI = 3.14
+ACTIVE = "A"
