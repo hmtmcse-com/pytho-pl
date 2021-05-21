7a67f9ee9cab2c4c523360946ad0866599c8ce41
hmtmcse <hmtm.cse@gmail.com>
2021-05-21 21:06:26 +0600

    comment

---
+++ comment.py
@@ -0,0 +1,16 @@
+
+# This is a comment of python
+
+# print is responsible for write/show something into console
+print("This is Bismillah Program")  # We can add comment in same line of code execution
+
+
+"""
+Example of Multiline comments
+The sum method is responsible for addition of 2 number and return them
+"""
+def sum(first, second):
+    return first + second
+
+
+print(sum(10, 20))
